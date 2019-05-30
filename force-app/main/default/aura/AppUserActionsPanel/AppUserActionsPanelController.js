ff83f55c807fcb2d982a96990fd9bf0632e6e58b
Andrii Olexandrovich <awispack@gmail.com>
2019-05-30 21:25:07 +0300

    Added funtionality to delete app user.

--- force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelController.js
+++ force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelController.js
@@ -6,6 +6,10 @@
         helper.performServerCall(component, "c.reactivateUser");
     },
     deleteAppUser: function (component, event, helper) {
-        helper.performServerCall(component, "c.deleteUser");
+        helper.performServerCall(component, "c.deleteUser")
+            .then($A.getCallback(_ => {
+                const workspaceAPI = component.find("workspace");
+                workspaceAPI.getFocusedTabInfo().then(response => workspaceAPI.closeTab({tabId: response.tabId}));
+            }));
     }
 });
--- force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelHelper.js
+++ force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelHelper.js
@@ -1,22 +1,19 @@
 ({
     performServerCall: function (component, controllerMethodName) {
         this.toggleComponentState(component);
-        const notifLib = component.find("notifLib");
-        component.find("recordLoader").reloadRecord(false, _ => {
-            const appUser = component.get("v._appUser");
-            this.performAction(component, controllerMethodName, {appUser})
-                .then($A.getCallback(_ => notifLib.showToast({
-                    variant: "success", title: "Done",
-                    message: "User was successfully updated.",
-                })))
-                .catch($A.getCallback(error => notifLib.showNotice({
-                    variant: "error", header: "Error Occurred",
-                    title: "Details", message: error[0].message,
-                })))
-                .then($A.getCallback(_ => {
-                    $A.get('e.force:refreshView').fire();
-                    this.toggleComponentState(component);
-                }));
-        });
+        const notifLib = component.find("notifLib"), appUser = component.get("v._appUser");
+        return this.performAction(component, controllerMethodName, {appUser})
+            .then($A.getCallback(_ => notifLib.showToast({
+                variant: "success", title: "Done",
+                message: "User was successfully updated.",
+            })))
+            .catch($A.getCallback(error => notifLib.showNotice({
+                variant: "error", header: "Error Occurred",
+                title: "Details", message: error[0].message,
+            })))
+            .then($A.getCallback(_ => {
+                $A.get('e.force:refreshView').fire();
+                this.toggleComponentState(component);
+            }));
     }
 });
