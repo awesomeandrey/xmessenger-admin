2f1a5da7f80d82ac1a90c3c80acf092ccf268591
Andrii Olexandrovich <awispack@gmail.com>
2019-05-30 20:10:20 +0300

    Establishing basic model.

--- force-app/main/default/aura/AppUserDeactivator/AppUserDeactivatorController.js
+++ force-app/main/default/aura/AppUserDeactivator/AppUserDeactivatorController.js
@@ -1,15 +1,25 @@
 ({
-    deactivateUser: function (component, event, helper) {
+    deactivateAppUser: function (component, event, helper) {
         helper.toggleComponentState(component);
-        const appUser = component.get("v._appUser");
-
-        debugger;
-
-        appUser.User_Id__c = 520;
-
-        helper.performAction(component, "c.deactivateUser", null)
-            .then($A.getCallback(response => {
-                debugger;
-            }));
+        const notifLib = component.find("notifLib");
+        component.find("recordLoader").reloadRecord(false, _ => {
+            const appUser = component.get("v._appUser");
+            helper.performAction(component, "c.deactivateUser", {appUser})
+                .then($A.getCallback(_ => {
+                    notifLib.showToast({
+                        variant: "success", title: "Done",
+                        message: "User was successfully deactivated.",
+                    });
+                    $A.get('e.force:refreshView').fire();
+                    $A.get("e.force:closeQuickAction").fire();
+                }))
+                .catch($A.getCallback(error => {
+                    notifLib.showNotice({
+                        variant: "error", header: "Error Occurred",
+                        title: "Details", message: error[0].message,
+                    });
+                    helper.toggleComponentState(component);
+                }));
+        });
     }
 });
