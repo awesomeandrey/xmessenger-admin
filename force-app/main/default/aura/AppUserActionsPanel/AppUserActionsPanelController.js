df2bd44be8f37d5798907a3eedc5140aa9c4f521
Andrii Olexandrovich <awispack@gmail.com>
2019-05-30 21:45:07 +0300

    Minor adjustments.

--- force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelController.js
+++ force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelController.js
@@ -8,8 +8,10 @@
     deleteAppUser: function (component, event, helper) {
         helper.performServerCall(component, "c.deleteUser")
             .then($A.getCallback(_ => {
-                const workspaceAPI = component.find("workspace");
-                workspaceAPI.getFocusedTabInfo().then(response => workspaceAPI.closeTab({tabId: response.tabId}));
+                const error = component.get("v.error"), workspaceAPI = component.find("workspace");
+                if ($A.util.isEmpty(error)) {
+                    workspaceAPI.getFocusedTabInfo().then(response => workspaceAPI.closeTab({tabId: response.tabId}));
+                }
             }));
     }
 });
--- force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelHelper.js
+++ force-app/main/default/aura/AppUserActionsPanel/AppUserActionsPanelHelper.js
@@ -3,14 +3,21 @@
         this.toggleComponentState(component);
         const notifLib = component.find("notifLib"), appUser = component.get("v._appUser");
         return this.performAction(component, controllerMethodName, {appUser})
-            .then($A.getCallback(_ => notifLib.showToast({
-                variant: "success", title: "Done",
-                message: "User was successfully updated.",
-            })))
-            .catch($A.getCallback(error => notifLib.showNotice({
-                variant: "error", header: "Error Occurred",
-                title: "Details", message: error[0].message,
-            })))
+            .then($A.getCallback(_ => {
+                component.set("v._error", {});
+                notifLib.showToast({
+                    variant: "success", title: "Done",
+                    message: "User was successfully updated.",
+                });
+            }))
+            .catch($A.getCallback(error => {
+                let errorObj = error[0];
+                component.set("v._error", errorObj);
+                notifLib.showNotice({
+                    variant: "error", header: "Error Occurred",
+                    title: "Details", message: errorObj.message,
+                });
+            }))
             .then($A.getCallback(_ => {
                 $A.get('e.force:refreshView').fire();
                 this.toggleComponentState(component);
