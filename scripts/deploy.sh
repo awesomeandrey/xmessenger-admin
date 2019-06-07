d42c566c89151ca38eea91a3e5a3069e86bc6315
Andrii Olexandrovich <awispack@gmail.com>
2019-06-07 14:23:48 +0300

    Adjusted scripts.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,22 +3,14 @@ while getopts ":u:" opt; do
   case $opt in
     u)
         export ORG_NAME=$OPTARG
-
-        printf "***DEPLOYMENT INITIATED***\n\n"
-
-        printf "Target org name: %s\n\n" "$ORG_NAME"
-
-        #Deploy via Metadata API;        
+        #Deploy via Metadata API;
         export tmp_dir=mdapioutput
         sfdx force:org:display -u $ORG_NAME
         sfdx force:source:convert -d $tmp_dir/
         sfdx force:mdapi:deploy -d $tmp_dir/ -u $ORG_NAME -w 100
         rm -rf $tmp_dir
-        
         #Run all tests in org;
         sfdx force:apex:test:run -u $ORG_NAME --wait 10
-
-        printf "***DEPLOYMENT COMPLETED***"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
