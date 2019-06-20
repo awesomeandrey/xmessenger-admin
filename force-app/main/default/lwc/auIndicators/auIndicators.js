484c24cdede75be343d5175a8e1cc94acf576814
Andrii Olexandrovich <awispack@gmail.com>
2019-06-20 20:27:45 +0300

    Adjusting lwc.

--- force-app/main/default/lwc/auIndicators/auIndicators.js
+++ force-app/main/default/lwc/auIndicators/auIndicators.js
@@ -1,34 +1,34 @@
 import {LightningElement, track} from "lwc";
+import {ShowToastEvent} from "lightning/platformShowToastEvent";
+import getUsersOnline from "@salesforce/apex/AdminController.getUsersOnline";
 
 export default class AuIndicators extends LightningElement {
     @track appUsers = [];
     @track loading = false;
 
     handleRefreshIndicators() {
-        debugger;
         this.loading = true;
-        setTimeout(_ => {
+        getUsersOnline().then(result => {
+            this.appUsers = result;
+        }).catch(error => {
+            this.appUsers = [];
+            console.error(">>>", JSON.stringify(error));
+            this.alert({
+                variant: "error",
+                title: "Could not load user indicators",
+                message: "Please, refer to console log for error details."
+            });
+        }).then(_ => {
             this.loading = false;
-            this.appUsers = [
-                {
-                    Id: "x001x0000008Ry9AAE",
-                    Name__c: "Test",
-                    Username__c: "test",
-                },
-                {
-                    Id: "x001x0000008Ry9AAE",
-                    Name__c: "Test",
-                    Username__c: "test",
-                }, {
-                    Id: "x001x0000008Ry9AAE",
-                    Name__c: "Test",
-                    Username__c: "test",
-                }
-            ];
-        }, 2500);
+        });
     }
 
     connectedCallback() {
         this.handleRefreshIndicators();
+    }
+
+    alert(toastDetails) {
+        const event = new ShowToastEvent(toastDetails);
+        this.dispatchEvent(event);
     }
 }
