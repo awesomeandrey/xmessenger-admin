b760464120c29afe88f7bf8c5b74d894d3a362b8
Andrii Olexandrovich <awispack@gmail.com>
2019-07-31 22:13:18 +0300

    Adjusted CI/CD scripts to execute one by one.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,10 @@ while getopts ":u:" opt; do
         export ORG_NAME=$OPTARG
         #Deploy via Metadata API;
         export tmp_dir=mdapioutput
-        sfdx force:org:display -u $ORG_NAME
-        sfdx force:source:convert -d $tmp_dir/
-        sfdx force:mdapi:deploy -d $tmp_dir/ -u $ORG_NAME -w 100
-        rm -rf $tmp_dir
+        sfdx force:org:display -u $ORG_NAME &&
+        sfdx force:source:convert -d $tmp_dir/ &&
+        sfdx force:mdapi:deploy -d $tmp_dir/ -u $ORG_NAME -w 100 &&
+        rm -rf $tmp_dir &&
         #Run all tests in org;
         sfdx force:apex:test:run -u $ORG_NAME --wait 10
       ;;
