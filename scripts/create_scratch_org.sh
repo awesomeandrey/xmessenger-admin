f6de0ef0086da2f4afef62819f052b5065b83656
Andrii Olexandrovich <awispack@gmail.com>
2019-08-02 19:17:33 +0300

    Refactored automation scripts.

--- scripts/create_scratch_org.sh
+++ scripts/create_scratch_org.sh
@@ -1,36 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#Sample usage: bash scripts/create_scratch_org.sh -u dev
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -a <scratch_org_alias> -u <target_dev_hub_username> "
+   echo -e "\t-a Scratch org alias."
+   echo -e "\t-u Connected DevHub username."
+   exit 1 # Exit script after printing help;
+}
 
-while getopts ":u:" opt; do
-  case $opt in
-    u)
-        #Define context constants;
-        export ORG_NAME=$OPTARG
-        export TARGET_DEV_HUB_USERNAME=andrii.melnichuk@xmessenger.com
-        #Create scratch org;
-        sfdx force:org:create \
-            -f config/project-scratch-def.json \
-            -a ${ORG_NAME} \
-            -v $TARGET_DEV_HUB_USERNAME &&
-        #Push source to scratch org (cleaning '.forceignore' file);
-        > .forceignore &&
-        sfdx force:source:push -u ${ORG_NAME} -f &&
-        #Create scratch org user;
-        sfdx force:user:create \
-            -u ${ORG_NAME} \
-            -f config/dev-user-def.json \
-            Username=${TARGET_DEV_HUB_USERNAME} &&
-        #Open scratch org;
-        sfdx force:org:open -u ${ORG_NAME}
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires target scratch org name." >&2
-      exit 1
-      ;;
-  esac
+while getopts "a:u:" opt
+do
+   case "$opt" in
+      a ) scratchOrgAlias="$OPTARG" ;;
+      u ) devHubUsername="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent;
+   esac
 done
+
+# Print helpFunction in case parameters are empty;
+if [[ -z "$scratchOrgAlias" ]] || [[ -z "$devHubUsername" ]]
+then
+   echo "Some or all of the parameters are empty.";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct;
+echo Scratch org alias is: "$scratchOrgAlias"
+echo DevHub username is: "$devHubUsername"
+
+#Create scratch org;
+sfdx force:org:create \
+    -f config/project-scratch-def.json \
+    -a "$scratchOrgAlias" \
+    -v "$devHubUsername" &&
+
+#Push source to scratch org (cleaning '.forceignore' file);
+> .forceignore &&
+sfdx force:source:push -u "$scratchOrgAlias" -f &&
+
+#Create scratch org user;
+sfdx force:user:create \
+    -u "$scratchOrgAlias" \
+    -f config/dev-user-def.json &&
+
+#Open scratch org;
+sfdx force:org:open -u "$scratchOrgAlias"
